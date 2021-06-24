c1be16a5f40b4ac7a1478d5ee6ae3a881243d5fd
Aariq <scottericr@gmail.com>
2021-06-24 13:38:37 -0400

    remove plot ID because it's correlated with current treatment

--- R/fit_models.R
+++ R/fit_models.R
@@ -4,6 +4,7 @@ fit_ht <- function(data, log_trans = TRUE) {
   } else {
     y = "height"
   }
-  f <- formula(paste0(y, "~ days * hist_treat * curr_treat + july_herbivory + (1|plot_id) + (1|pot_id) + (1|plant_id)"))
+  #plot_id isn't i.i.d. because it's related to the current treatment.  Can't include as random effect.
+  f <- formula(paste0(y, "~ days * hist_treat * curr_treat + july_herbivory + (1|pot_id) + (1|plant_id)"))
   k_ht <- lmer(f, data)
 }

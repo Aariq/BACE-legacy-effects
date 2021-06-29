e84582c78dc5c0ae31c06fde50aa666d0bfdf1a0
Aariq <scottericr@gmail.com>
2021-06-29 16:51:18 -0400

    added "house" as fixed effect (#11)

--- R/fit_models.R
+++ R/fit_models.R
@@ -4,7 +4,7 @@ fit_ht <- function(data, log_trans = TRUE) {
   } else {
     y = "height"
   }
-  #plot_id isn't i.i.d. because it's related to the current treatment.  Can't include as random effect.
-  f <- formula(paste0(y, "~ days * hist_treat * curr_treat + july_herbivory + (1|pot_id) + (1|plant_id)"))
+  #too few levels of house to include as random effect.
+  f <- formula(paste0(y, "~ days * hist_treat * curr_treat + july_herbivory + house + (1|pot_id) + (1|plant_id)"))
   k_ht <- lmer(f, data)
 }
--- R/plot_fitted.R
+++ R/plot_fitted.R
@@ -4,6 +4,7 @@ my_augment <- function(model) {
                           curr_treat = factor(levels(mf$curr_treat), levels(mf$curr_treat)),
                           days = seq(min(mf$days), max(mf$days), length.out = 50),
                           july_herbivory = 0,
+                          house = c("1", "2", "3"), 
                           plot_id = "newlevel",
                           pot_id = "newlevel",
                           plant_id = "newlevel")

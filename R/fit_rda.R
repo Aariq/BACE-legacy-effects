5d95731c0e8f4577b278a12c990aef011418c27c
Aariq <scottericr@gmail.com>
2021-07-08 13:02:21 -0400

    added conditional effect of house to RDA

--- R/fit_rda.R
+++ R/fit_rda.R
@@ -1,16 +1,14 @@
 #scale nutrient data and create a matrix column
 prep_rda_data <- function(data) {
-  nutr_df <-
-    data %>% 
+  data %>% 
     mutate(across(c(n, ca, k, mg, p, al, b, cu, fe, mn, zn), ~scale(.x), .names = "{.col}_scaled")) %>%
     mutate(nutr = as.matrix(select(., ends_with("_scaled"))))
-  return(nutr_df)
 }
 
 #fit RDA using prepped data
 fit_rda <- function(nutr_df) {
-  crop_rda <- rda(nutr_df["nutr"] ~ historical*current + jul_herbivory + late_herbivory, 
-                  data = nutr_df,
-                  scale = FALSE)
-  return(crop_rda)
+  rda(nutr_df["nutr"] ~ historical*current + jul_herbivory + late_herbivory +
+        Condition(house), 
+      data = nutr_df,
+      scale = FALSE)
 }
